fec6775ab191d81e6b93a9761a2ac8396c390970
compott <gitanaperskenej@gmail.com>
2026-03-05 19:27:48 +0200

    code style change

--- src/hooks/useLanguage.js
+++ src/hooks/useLanguage.js
@@ -1,6 +1,6 @@
-import { useCallback } from 'react';
-import { useTranslation } from 'react-i18next';
-import { LANG_STORAGE_KEY, LANGS, DEFAULT_LANG } from '../constants.js';
+import { useCallback } from "react";
+import { useTranslation } from "react-i18next";
+import { LANG_STORAGE_KEY, LANGS, DEFAULT_LANG } from "../constants.js";
 
 export function useLanguage() {
   const { i18n } = useTranslation();
@@ -12,7 +12,7 @@ export function useLanguage() {
       i18n.changeLanguage(code);
       try {
         localStorage.setItem(LANG_STORAGE_KEY, code);
-        if (typeof document !== 'undefined') document.documentElement.lang = code;
+        if (typeof document !== "undefined") document.documentElement.lang = code;
       } catch (e) {}
     },
     [i18n]
